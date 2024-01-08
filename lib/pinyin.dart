06f5cf0bdad3225ced2c3a0c747aca2519def5a4
Sun Jiao <P.ozzy@outlook.com>
2024-01-08 15:38:13 +0800

    Update pinyin.dart
    add export

--- lib/pinyin.dart
+++ lib/pinyin.dart
@@ -1,6 +1,9 @@
 library pinyin;
 
-export 'src/dict_data.dart';
+export 'dict/multi_pinyin_dict.dart';
+export 'dict/pinyin_dict.dart';
+export 'dict/pinyin_to_zhuyin_dict.dart';
+export 'dict/simp_trad_dict.dart';
 export 'src/pinyin_format.dart';
 export 'src/pinyin_helper.dart';
 export 'src/pinyin_exception.dart';
